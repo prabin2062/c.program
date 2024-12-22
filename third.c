8294e740eb398f3e2cead5db71b03137405c2fab
prabinbhatt <bhattsamir2062@gmail.com>
2024-12-22 15:30:37 +0545

    fourth commit

---
+++ third.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,diff;
+    printf("Enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    diff=a-b;
+    printf("The difference of %d and %d is %d\n",a,b,diff);
+    return 0;
+}
