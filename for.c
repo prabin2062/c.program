5ad83470b1784451742f10dfa5295b08232e9b07
prabinbhatt <bhattsamir2062@gmail.com>
2024-12-22 15:43:04 +0545

    fix: :memo: I have added the code for the for loop

---
+++ for.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int num1, num2, count=10;
+
+    printf("Enter two integers: ");
+   for (size_t i = 0; i < count; i++)
+   {
+    printf("I am pravin \n");
+   }
+   
+}
