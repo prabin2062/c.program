4b0c608ba093975cc4834d2a96a3e49f26527bc6
prabinbhatt <bhattsamir2062@gmail.com>
2024-12-24 06:55:16 +0545

    feat: :art: I have added a code to find simple interest

---
+++ Program1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float calculateSimpleInterest(float principal, float rate, float time);
+
+int main() {
+    float principal, rate, time, interest;
+
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter annual interest rate (in percentage): ");
+    scanf("%f", &rate);
+
+    printf("Enter time (in years): ");
+    scanf("%f", &time);
+
+    interest = calculateSimpleInterest(principal, rate, time);
+
+    printf("Simple Interest = %.2f\n", interest);
+
+    return 0;
+}
+
+float calculateSimpleInterest(float principal, float rate, float time) {
+    return (principal * rate * time) / 100;
+// }
